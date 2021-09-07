a16bf907280444ad48c1020a2a0dced177f95bb6
choimory <43328589+choimory@users.noreply.github.com>
2021-09-08 00:30:43 +0900

    Initial commit

---
+++ src/main/java/com/choimory/hellojavacrawling/HelloJavaCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.hellojavacrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaCrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJavaCrawlingApplication.class, args);
+    }
+
+}
