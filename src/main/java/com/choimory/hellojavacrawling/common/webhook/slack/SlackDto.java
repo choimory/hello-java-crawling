243a11ef94989a4febb02d364e2acf8be5c47f1b
choimory <choimory@naver.com>
2021-09-27 11:25:25 +0900

    Slack 웹훅 DTO 추가

--- src/main/java/com/choimory/hellojavacrawling/common/webhook/slack/SlackDto.java
+++ src/main/java/com/choimory/hellojavacrawling/common/webhook/slack/SlackDto.java
@@ -4,5 +4,9 @@ import lombok.Builder;
 
 @Builder
 public class SlackDto {
+    private final String channel;
+    private final String username;
+    private final String icon_url;
+    private final String icon_emoji;
     private final String text;
 }
