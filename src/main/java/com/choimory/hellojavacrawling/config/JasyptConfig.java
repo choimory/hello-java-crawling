4dcb753fea8dc74aad7b5703107ef4f86dc21d88
choimory <choimory@naver.com>
2021-09-27 14:42:44 +0900

    jasypt encryptor

---
+++ src/main/java/com/choimory/hellojavacrawling/config/JasyptConfig.java
@@ -0,0 +1,33 @@
+package com.choimory.hellojavacrawling.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class JasyptConfig {
+    @Value("${jasypt.encryptor.key}")
+    private String key;
+
+    @Bean("jasyptStringEncrptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        log.info(encryptor.encrypt("https://hooks.slack.com/services/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"));
+
+        return encryptor;
+    }
+}
